7c3d7779925704d4822da80457cefbcf39fba76f
Yroslav <dub4step@gmail.com>
2023-08-24 02:05:53 +0500

    Изменил компонент Nav

--- components/Nav.js
+++ components/Nav.js
@@ -1,16 +1,21 @@
 import Link from "next/link";
-import css from "./Nav.module.css"
+import css from "./Nav.module.css";
+
+const pages = [
+  { href: "/home", name: "Home" },
+  { href: "/todolist", name: "To Do List" },
+  { href: "/table", name: "Table users" },
+];
 
 export default function Nav() {
   return (
     <nav className={css.nav}>
       <ul>
-        <li>
-          <Link href="/home">Home</Link>
-        </li>
-        <li>
-          <Link href="/todolist">To Do List</Link>
-        </li>
+        {pages.map(({ href, name }) => (
+          <li>
+            <Link href={href}>{name}</Link>
+          </li>
+        ))}
       </ul>
     </nav>
   );
