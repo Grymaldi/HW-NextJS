ccd57d05af7701aca6ac265c708cc95e5b60df0b
Yroslav <dub4step@gmail.com>
2023-08-26 20:51:55 +0500

    исправление таблицы

--- components/ListUsers.js
+++ components/ListUsers.js
@@ -2,14 +2,14 @@ import FetchUser from "../components/FetchUser";
 import { useState } from "react";
 
 export default function ListUser() {
-  const [stateUsersIds, setUsers] = useState(
+  const [stateUsersIds] = useState(
     [...Array(10)].map((_, i) => 1 + i)
   );
   return (
-    <div className="user-table">
+    <>
       {stateUsersIds.map((id) => (
         <FetchUser id={id} key={id} />
       ))}
-    </div>
+    </>
   );
 }
